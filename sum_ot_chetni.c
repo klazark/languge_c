f2af26b782dcc7602b802ec6de65d595945fdc89
kdobrik <kdobrik@gmail.com>
2025-12-28 20:57:56 +0200

    gotova programa XD

--- sum_ot_chetni.c
+++ sum_ot_chetni.c
@@ -2,11 +2,29 @@
 
 
 int main(void) {
-int n;//broi chisla
-int sum;
-int a;
-printf("vyvedi n =");
-scanf("%d", &n);
+    int n;//broi chisla
+    int sum;
+    int a;
+
+    printf("vyvedi n =");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        prinf("a=");
+        scanf("%d", &a);
+        if(a%2==0) {
+            sum = sum + a;
+        }
+    }
+
+    if(sum == 0){
+        prinf("Nqama takiva chisla")
+    } else {
+        printf("sum=%d", sum);
+
+    }
+//a%2== 0
+//sum = sum+a;
 }
-a%2== 0
-sum = sum+a;
+
+
